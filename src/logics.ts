f8a662a19d050ab043f7e232ec703d223b19864d
aleviannaf <alexandrevianna511@gmail.com>
2023-07-11 16:33:31 -0400

    factory (createchocolate): aplication of the pg-formart to insert on the table

--- src/logics.ts
+++ src/logics.ts
@@ -2,27 +2,25 @@ import { Request, Response } from "express"
 import { client } from "./database"
 import { QueryConfig, QueryResult } from "pg"
 import { TChocolate, TChocolateRequest } from "./interfaces"
+import format from "pg-format"
 const createChocolates = async (request: Request, response: Response): Promise<Response> =>{
 
     const chocolatesData: TChocolateRequest = request.body
 
-    const queryString = `
+    const queryString = format(`
         INSERT INTO 
             chocolates
-            (type, weight, price, cocoa_percentage, in_stock, more_ingredients) 
+            (%I) 
         VALUES 
-            ($1, $2, $3, $4, $5, $6) 
+            (%L) 
         RETURNING *;
-    `
+    `,
+    Object.keys(chocolatesData),
+    Object.values(chocolatesData)
+    )
 
-    const queryConfig: QueryConfig = {
-        text: queryString,
-        values: Object.values(chocolatesData)
-    }
 
-    const queryResult: QueryResult<TChocolate> = await client.query(queryConfig)
-
-    console.log(queryConfig)
+    const queryResult: QueryResult<TChocolate> = await client.query(queryString)
 
     return response.status(201).json(queryResult.rows[0])
 }
